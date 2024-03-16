76f87ce351439bbe5bf7921678bb6bbf1669db03
javoliang <137265733+javoliang@users.noreply.github.com>
2024-03-15 21:48:45 -0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,8 +1,6 @@
-URL_SERVICE = "https://cnt-58348162-6591-469d-8ca6-1a1ac828505d.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-0d3e5536-9c87-4a26-86d9-a754c8bfe03c.containerhub.tripleten-services.com"
 # Url del servidor Tripleten
 CREATE_USER_PATH = "/api/v1/users/"
 # Almacena la ruta para crear un usuario o usuaria en esta variable
-PRODUCTS_KITS_PATH = "/api/v1/products/kits"
-# Almacena la ruta para crear un kit de productos personales en esta variable
 KITS_PATH = "/api/v1/kits"
 # Almacena la ruta para crear un kit en esta variable
