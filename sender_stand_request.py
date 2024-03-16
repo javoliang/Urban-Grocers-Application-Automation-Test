f3f5488426e9b3f1da759134394a374539c7fd33
javoliang <137265733+javoliang@users.noreply.github.com>
2024-03-15 21:49:44 -0500

    Update sender_stand_request.py

---
+++ sender_stand_request.py
@@ -0,0 +1,42 @@
+import configuration
+import requests
+import data
+
+
+def post_new_user(body):
+    return requests.post(
+        configuration.URL_SERVICE +
+        configuration.CREATE_USER_PATH,
+        json=body,
+        headers=data.headers,
+    )
+
+
+response = post_new_user(data.user_body)
+print(response.status_code)
+print(response.json())
+
+
+def auth_token():
+    user = post_new_user(data.user_body)
+    user_json = user.json()
+
+    return user_json['authToken']
+
+
+def post_new_client_kit(kit_body):
+    token = auth_token()
+    headers = data.headers.copy()
+    headers['Authorization'] = f'Bearer {token}'
+
+    return requests.post(
+        configuration.URL_SERVICE +
+        configuration.KITS_PATH,
+        json=kit_body,
+        headers=headers,
+    )
+
+
+response = post_new_client_kit(data.kit_body)
+print(response.status_code)
+print(response.json())
