4c6dc753ed138075f82bd4ea08d0a94284851086
javoliang <137265733+javoliang@users.noreply.github.com>
2024-03-17 17:22:45 -0500

    Update data.py

--- data.py
+++ data.py
@@ -8,6 +8,50 @@ user_body = {
             "address": "8042 Lancaster Ave.Hamburg, NY"
 }
 
-kit_body = {
-    "name": "TripleTen"
+def get_kit_body(name):
+    return kit_bodies[name].copy()
+
+kit_bodies = {
+    "one_character": {"name": "a"},
+    "maximum_characters": {"name": ("Abcdabcdabcdabcdabcdabcdabcdabcdab"
+                                    "cdabcdabcdabcdabcdabcdabcdabcdabcd"
+                                    "abcdabcdabcdabcdabcdabcdabcdabcdab"
+                                    "cdabcdabcdabcdabcdabcdabcdabcdabcd"
+                                    "abcdabcdabcdabcdabcdabcdabcdabcdab"
+                                    "cdabcdabcdabcdabcdabcdabcdabcdabcd"
+                                    "abcdabcdabcdabcdabcdabcdabcdabcdab"
+                                    "cdabcdabcdabcdabcdAbcdabcdabcdabcd"
+                                    "abcdabcdabcdabcdabcdabcdabcdabcdab"
+                                    "cdabcdabcdabcdabcdabcdabcdabcdabcd"
+                                    "abcdabcdabcdabcdabcdabcdabcdabcdab"
+                                    "cdabcdabcdabcdabcdabcdabcdabcdabcd"
+                                    "abcdabcdabcdabcdabcdabcdabcdabcdab"
+                                    "cdabcdabcdabcdabcdabcdabcdabcdabcd"
+                                    "abcdabcdabcdabcdabcdabcdabcdabcdabC")},
+    "empty_string": {"name": ""},
+    "maximum_plus_one_characters": {"name": ("Abcdabcdabcdabcdabcdabcdab"
+                                             "cdabcdabcdabcdabcdabcdabcd"
+                                             "abcdabcdabcdabcdabcdabcdab"
+                                             "cdabcdabcdabcdabcdabcdabcd"
+                                             "abcdabcdabcdabcdabcdabcdab"
+                                             "cdabcdabcdabcdabcdabcdabcd"
+                                             "abcdabcdabcdabcdabcdabcdab"
+                                             "cdabcdabcdabcdabcdabcdabcd"
+                                             "abcdabcdabcdabcdabcdabcdab"
+                                             "cdabcdabcdabcdabcdabcdAbcd"
+                                             "abcdabcdabcdabcdabcdabcdab"
+                                             "cdabcdabcdabcdabcdabcdabcd"
+                                             "abcdabcdabcdabcdabcdabcdab"
+                                             "cdabcdabcdabcdabcdabcdabcd"
+                                             "abcdabcdabcdabcdabcdabcdab"
+                                             "cdabcdabcdabcdabcdabcdabcd"
+                                             "abcdabcdabcdabcdabcdabcdab"
+                                             "cdabcdabcdabcdabcdabcdabcd"
+                                             "abcdabcdabcdabcdabcdabcdab"
+                                             "cdabcdabcdabcdabC")},
+    "special_characters": {"name": '"№%@"'},
+    "spaces": {"name": " A Aaa "},
+    "numbers": {"name": "123"},
+    "empty_kit_body": {},
+    "number_name": {"name": 123}
 }
