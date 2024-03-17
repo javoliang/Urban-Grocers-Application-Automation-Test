fec36bf409f563177564cc1256f04b0dc3edb227
javoliang <137265733+javoliang@users.noreply.github.com>
2024-03-17 17:21:30 -0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-0d3e5536-9c87-4a26-86d9-a754c8bfe03c.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-82a98e1b-445d-4521-9332-8602f15dceb7.containerhub.tripleten-services.com"
 # Url del servidor Tripleten
 CREATE_USER_PATH = "/api/v1/users/"
 # Almacena la ruta para crear un usuario o usuaria en esta variable
